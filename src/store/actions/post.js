94efb5d8fec1095a77e0894580a445b24c4eb328
Ivan <prolotka@gmail.com>
2020-10-09 19:48:01 +0300

    add get posts and create post by using db

--- src/store/actions/post.js
+++ src/store/actions/post.js
@@ -1,9 +1,14 @@
 import {ADD_POST, LOAD_POSTS, REMOVE_POST, TOGGLE_BOOKED} from "../types";
+import {DB} from "../../db";
+import * as FileSystem from 'expo-file-system';
 
 export const loadPosts = () => {
-    return {
-        type: LOAD_POSTS,
-        payload: []
+    return async dispatch => {
+        const posts = await DB.getPosts()
+        dispatch({
+            type: LOAD_POSTS,
+            payload: posts
+        })
     }
 }
 
@@ -21,10 +26,24 @@ export const removePost = id => {
 
     }
 }
-export const addPost = post => {
-    post.id = Date.now().toString()
-    return {
-        type: ADD_POST,
-        payload: post
+export const addPost = post => async dispatch => {
+    const fileName = post.img.split('/').pop()
+    const newPath = FileSystem.documentDirectory + fileName
+
+    try {
+        await FileSystem.moveAsync({
+            to: newPath,
+            from: post.img
+        })
+    } catch (e) {
+        console.log(e)
     }
+
+    const payload = {...post, img: newPath}
+    payload.id = await DB.createPost(payload)
+
+    dispatch({
+        type: ADD_POST,
+        payload
+    })
 }
